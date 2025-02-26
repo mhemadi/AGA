3a415b36690a54046c515df115190c8605608832
mhemadi <mh_emadie@yahoo.com>
2025-02-25 20:46:28 -0700

    feat: Allow Cors

--- AltaGasAssignment.WebApi/Program.cs
+++ AltaGasAssignment.WebApi/Program.cs
@@ -4,7 +4,18 @@ using AltaGasAssignment.WebApi.Endpoints;
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddServices(builder.Configuration);
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: "AllowAll", policy =>
+    {
+        policy.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod();
+    });
+});
+
 var app = builder.Build();
+
+app.UseCors("AllowAll");
+
 app.MapFileUploadEndpoints();
 app.MapTripEndpoints();
 app.MapInitializeEndpoints();
