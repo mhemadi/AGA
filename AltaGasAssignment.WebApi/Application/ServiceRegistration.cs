efa6f204af9ab20b651cee32ca9de4a060378c61
mhemadi <mh_emadie@yahoo.com>
2025-02-25 17:47:09 -0700

    feat: Apply db migration

--- AltaGasAssignment.WebApi/Application/ServiceRegistration.cs
+++ AltaGasAssignment.WebApi/Application/ServiceRegistration.cs
@@ -1,10 +1,15 @@
-﻿namespace AltaGasAssignment.WebApi.Application
+﻿using AltaGasAssignment.WebApi.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace AltaGasAssignment.WebApi.Application
 {
     public static class ServiceRegistration
     {
         public static IServiceCollection AddServices(this IServiceCollection services,
             IConfiguration configuration)
         {
+            services.AddDbContext<AppDbContext>(options =>
+                options.UseSqlite(configuration.GetConnectionString("DefaultConnection")));
 
             return services;
         }
---
+++ AltaGasAssignment.WebApi/Data/Migrations/20250226004515_Initial_Setup.cs
@@ -0,0 +1,197 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AltaGasAssignment.WebApi.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial_Setup : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Cities",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    CityId = table.Column<int>(type: "INTEGER", nullable: false),
+                    Name = table.Column<string>(type: "varchar(100)", nullable: false),
+                    TimeZoneStr = table.Column<string>(type: "varchar(100)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Cities", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "EquipmentEventTypes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Code = table.Column<string>(type: "varchar(10)", nullable: false),
+                    EventDescription = table.Column<string>(type: "varchar(100)", nullable: false),
+                    LongDescription = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_EquipmentEventTypes", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "FileUploads",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "TEXT", nullable: false),
+                    UploadDate = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    FileUploadType = table.Column<int>(type: "INTEGER", nullable: false),
+                    FileName = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_FileUploads", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Trips",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "TEXT", nullable: false),
+                    EquipmentId = table.Column<string>(type: "TEXT", nullable: false),
+                    OriginCityId = table.Column<int>(type: "INTEGER", nullable: false),
+                    DestinationCityId = table.Column<int>(type: "INTEGER", nullable: true),
+                    StartDate = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    EndDate = table.Column<DateTime>(type: "TEXT", nullable: true),
+                    TotalTripMinutes = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Trips", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Trips_Cities_DestinationCityId",
+                        column: x => x.DestinationCityId,
+                        principalTable: "Cities",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_Trips_Cities_OriginCityId",
+                        column: x => x.OriginCityId,
+                        principalTable: "Cities",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "EquipmentEvents",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "TEXT", nullable: false),
+                    EquipmentId = table.Column<string>(type: "TEXT", nullable: false),
+                    EquipmentEventTypeId = table.Column<int>(type: "INTEGER", nullable: false),
+                    EventDate = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    CityId = table.Column<int>(type: "INTEGER", nullable: false),
+                    TripId = table.Column<Guid>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_EquipmentEvents", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_EquipmentEvents_Cities_CityId",
+                        column: x => x.CityId,
+                        principalTable: "Cities",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_EquipmentEvents_EquipmentEventTypes_EquipmentEventTypeId",
+                        column: x => x.EquipmentEventTypeId,
+                        principalTable: "EquipmentEventTypes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_EquipmentEvents_Trips_TripId",
+                        column: x => x.TripId,
+                        principalTable: "Trips",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Cities_CityId",
+                table: "Cities",
+                column: "CityId",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_EquipmentEvents_CityId",
+                table: "EquipmentEvents",
+                column: "CityId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_EquipmentEvents_EquipmentEventTypeId",
+                table: "EquipmentEvents",
+                column: "EquipmentEventTypeId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_EquipmentEvents_TripId",
+                table: "EquipmentEvents",
+                column: "TripId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_EquipmentEventTypes_Code",
+                table: "EquipmentEventTypes",
+                column: "Code",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Trips_DestinationCityId",
+                table: "Trips",
+                column: "DestinationCityId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Trips_EndDate",
+                table: "Trips",
+                column: "EndDate");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Trips_EquipmentId",
+                table: "Trips",
+                column: "EquipmentId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Trips_OriginCityId",
+                table: "Trips",
+                column: "OriginCityId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Trips_StartDate",
+                table: "Trips",
+                column: "StartDate");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Trips_TotalTripMinutes",
+                table: "Trips",
+                column: "TotalTripMinutes");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "EquipmentEvents");
+
+            migrationBuilder.DropTable(
+                name: "FileUploads");
+
+            migrationBuilder.DropTable(
+                name: "EquipmentEventTypes");
+
+            migrationBuilder.DropTable(
+                name: "Trips");
+
+            migrationBuilder.DropTable(
+                name: "Cities");
+        }
+    }
+}
